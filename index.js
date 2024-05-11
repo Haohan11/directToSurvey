6452368c718a3273fbcd8b0ee9eec89d286e1700
HaoHan <haohan11pfer@protonmail.com>
2024-05-12 04:55:36 +0800

    add index.js with redirect feature

---
+++ index.js
@@ -0,0 +1,7 @@
+const surveyLinks = [
+    "https://github.com/Haohan11/directToSurvey",
+    "https://github.com/Haohan11/fitness-fortress",
+    "https://github.com/Haohan11/reactDemo",
+]
+
+window.location.assign(surveyLinks[Math.floor(Math.random() *3)])
